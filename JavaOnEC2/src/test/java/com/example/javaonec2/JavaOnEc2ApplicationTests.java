557f31e4a60c75b9ded7f6d37aef8316f0fbc43c
Akshat Sinha <akshatsinhasramhardy@gmail.com>
2025-09-01 20:43:51 +0530

    OK currect window

---
+++ JavaOnEC2/src/test/java/com/example/javaonec2/JavaOnEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaonec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOnEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
