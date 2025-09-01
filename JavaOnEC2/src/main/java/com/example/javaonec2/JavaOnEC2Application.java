91df6bef9c1a74bd33719aabbf0a917f1b25866e
Akshat Sinha <akshatsinhasramhardy@gmail.com>
2025-09-01 12:43:38 +0530

    Initial commit: Java on Cloud EC2 project

---
+++ JavaOnEC2/src/main/java/com/example/javaonec2/JavaOnEC2Application.java
@@ -0,0 +1,10 @@
+package com.example.javaonec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOnEC2Application {
+    public static void main(String[] args){SpringApplication.run(JavaOnEC2Application.class,args);}
+}
+
